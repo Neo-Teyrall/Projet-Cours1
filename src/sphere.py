1336325af69910771546955eefad3f6a12b382bd
Neo-Teyrall <mailliw.marg@gmail.com>
2020-09-10 22:47:05 +0200

    utilisationde de la librairy personnel Vector3 | calcule des voisin | structure ation du code

--- src/sphere.py
+++ src/sphere.py
@@ -1,10 +1,10 @@
 import sys
 import math
-import anti_lib
-from anti_lib import Vec
+import vector
+from vector import Vector3
 
 
-def calc_points(number_points : int, position : Vec, angle = None):
+def calc_points(number_points : int, position : Vector3, angle = None):
     """reprise de l'algoryhtme de sphère de saff copié et modifié depuis le
     packet anti_lib_progs"""
     points = []
@@ -23,9 +23,9 @@ def calc_points(number_points : int, position : Vec, angle = None):
         else:
             phi += 3.6 / math.sqrt(N * (1 - h * h))
 
-        points.append(Vec(math.sin(phi) * math.sin(theta),
-                      math.cos(phi) * math.sin(theta),
-                          -math.cos(theta)) + position)
+        points.append(Vector3(X = math.sin(phi) * math.sin(theta),
+                              Y = math.cos(phi) * math.sin(theta),
+                              Z = -math.cos(theta)) + position)
         phi %= 2*math.pi
 
     return points
@@ -33,7 +33,7 @@ def calc_points(number_points : int, position : Vec, angle = None):
 
 
 if __name__ == "__main__" :
-    points = calc_points(96, Vec(10,10,10))
+    points = calc_points(96, Vector3(10,10,10))
     for i, point in enumerate(points) :
         p = (point)
         print("{:6s}{:5d}{:^4s}{:1s}{:3s}{:1s}{:4d}{:1s}{:8.3f}{:8.3f}{:8.3f}\n"
--- src/vector.py
+++ src/vector.py
@@ -1,45 +1,51 @@
 import math
 
-class Vec3():
+class Vector3():
+    "class de vecteur a trois dimmension"
+
     def __init__(self,X=0,Y=0,Z=0, random = False):
         """initialisation"""
         self.x = float(X)
         self.y = float(Y)
         self.z = float(Z)
         pass
-    
+
+
     def __str__(self):
         """ sortie str """
-        return "{}\t{}\t{}".format(self.x,self.y,self.z)
+        return "({}\t{}\t{})".format(self.x,self.y,self.z)
+
 
     def __add__(self,add):
         """ addition de vector 3 """
         if isinstance(add, int) or isinstance(add, float):
-            return Vec3(self.x + add,
+            return Vector3(self.x + add,
                         self.y + add,
                         self.z + add)
         else : 
-            return Vec3(self.x + add.x,
+            return Vector3(self.x + add.x,
                         self.y + add.y,
                         self.z + add.z)
+
 
     def __sub__(self,sub):
         """ soustraction de vector 3 """
         if isinstance(sub, int) or isinstance(sub, float):
-            return Vec3(self.x - sub,
+            return Vector3(self.x - sub,
                         self.y - sub,
                         self.z - sub)
-        return Vec3(self.x - sub.x,
+        return Vector3(self.x - sub.x,
                     self.y - sub.y,
                     self.z - sub.z)
+
 
     def __mul__(self,mul):
         """ multiplication de vector 3 """
         if isinstance(mul, int) or isinstance(mul, float):
-            return Vec3(self.x * mul,
+            return Vector3(self.x * mul,
                         self.y * mul,
                         self.z * mul)
-        return Vec3(self.x * mul.x,
+        return Vector3(self.x * mul.x,
                     self.y * mul.y,
                     self.z * mul.z)
 
@@ -47,10 +53,10 @@ class Vec3():
         """ division   de vector 3 """
         print("floodiv",fdiv)
         if isinstance(fdiv, int) or isinstance(fdiv, float):
-            return Vec3(self.x // fdiv,
+            return Vector3(self.x // fdiv,
                         self.y // fdiv,
                         self.z // fdiv)
-        return Vec3(self.x // fdiv.x,
+        return Vector3(self.x // fdiv.x,
                     self.y // fdiv.y,
                     self.z // fdiv.z)
 
@@ -58,31 +64,36 @@ class Vec3():
         """ division vrais de vector 3 """
        # print("truediv",tdiv)
         if isinstance(tdiv, int) or isinstance(tdiv, float):
-            return Vec3(self.x / tdiv,
+            return Vector3(self.x / tdiv,
                         self.y / tdiv,
                         self.z / tdiv)
-        return Vec3(self.x / tdiv.x,
+        return Vector3(self.x / tdiv.x,
                     self.y / tdiv.y,
                     self.z / tdiv.z)
+
+
     def __pow__(self,p):
         """ puissance de vector 3 """
         if isinstance(p, int) or isinstance(p, float):
-            return Vec3(self.x ** p,
+            return Vector3(self.x ** p,
                         self.y ** p,
                         self.z ** p)
         exit( "Erreur, impossible de faire la puissance")
-        
+
+
     def dist_to(self,point):
+        "calcule de la distance entre 2 vecteur3"
         return math.sqrt((self.x - point.x)**2
                          + (self.y - point.y)**2
                          + (self.z - point.z)**2 )
     def norm(self):
+        "Calcule de la norm du vecteur3"
         return math.sqrt(self.x**2 + self.y**2 + self.z**2)
 
 
 if __name__ ==  "__main__":
-    a = Vec3(1,2,3)
-    b = Vec3(3,2,1)
+    a = Vector3(1,2,3)
+    b = Vector3(3,2,1)
     print("*", a + b )
     print("-", a - b )
     print("*", a * b )
